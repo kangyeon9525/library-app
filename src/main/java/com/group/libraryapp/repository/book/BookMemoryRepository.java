39694751bdec46253243869fbbac9e2978b07120
김강연 <kangyeon9525@gmail.com>
2026-03-13 23:42:35 +0900

    test: 스프링 컨테이너 테스트

---
+++ src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
@@ -0,0 +1,16 @@
+package com.group.libraryapp.repository.book;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookMemoryRepository implements BookRepository {
+
+    // private final List<Book> books = new ArrayList<Book>();
+
+    @Override
+    public void saveBook() {
+        System.out.println("MemoryRepository");
+        // books.add(new Book());
+    }
+}
