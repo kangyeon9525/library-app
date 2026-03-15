2fd1db80bdb742e562bc3f09577ff00263c40950
김강연 <kangyeon9525@gmail.com>
2026-03-15 12:55:44 +0900

    feat: Transaction 적용

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,24 +19,27 @@ public class UserServiceV2 {
     this.userRepository = userRepository;
   }
 
+  @Transactional
   public void saveUser(UserCreateRequest request) {
-    User u = userRepository.save(new User(request.getName(), request.getAge()));
+    userRepository.save(new User(request.getName(), request.getAge()));
   }
 
+  @Transactional
   public List<UserResponse> getUsers() {
     return userRepository.findAll().stream()
       .map(UserResponse::new)
       .collect(Collectors.toList());
   }
 
+  @Transactional
   public void updateUser(UserUpdateRequest request) {
     User user = userRepository.findById(request.getId())
       .orElseThrow(IllegalArgumentException::new);
 
     user.updateName(request.getName());
-    userRepository.save(user);
   }
 
+  @Transactional
   public void deleteUser(String name) {
     User user = userRepository.findByName(name)
       .orElseThrow(IllegalArgumentException::new);
